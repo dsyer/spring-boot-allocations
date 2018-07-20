3d2ded80be7da23675daacdc20df434ab72e06b1
Dave Syer <dsyer@pivotal.io>
2018-07-20 17:49:18 +0100

    Add web client to test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,17 +3,25 @@ package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DirtiesContext
+@AutoConfigureWebTestClient
 public class DemoApplicationTests {
+
+	@Autowired
+	private WebTestClient rest;
 
 	@Test
 	public void contextLoads() throws Exception {
+		rest.get().uri("/").exchange().expectBody(String.class).isEqualTo("Hello");
 	}
 
 }
